093187186f5d846791e163a2da9de03ea09e44ed
Ankit Das <ecryptoguru@gmail.com>
2023-07-27 15:48:54 +0530

    Deploy ERC20 using hardhat in goerli testnet

---
+++ scripts/deploy.js
@@ -0,0 +1,27 @@
+
+const { ethers } = require("hardhat")
+
+
+
+async function main() {
+  const [deployer] = await ethers.getSigners();
+
+  console.log("Deploying contracts with the account:", deployer.address);
+
+  const weiAmount = (await deployer.getBalance()).toString();
+  
+  console.log("Account balance:", (await ethers.utils.formatEther(weiAmount)));
+
+  // make sure to replace the "zaviour" reference with your own ERC-20 name!
+  const Token = await ethers.getContractFactory("zaviour");
+  const token = await Token.deploy();
+
+  console.log("Token address:", token.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+});
